26fabaeddcd505935e95ee6b89948b9d2a91a213
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-11-23 20:05:50 +0530

    receive function in contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,7 +3,8 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet{
     // storage variables
-    uint public funds = 1000; // only positive values
-    int public counter = -10;
+    // uint public funds = 1000; // only positive values
+   
+   receive() external payable{}
 
 }
