4a95fd09258b5022c8e818124712fd5de6f1d856
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-12-02 17:45:31 +0530

    lookUpTableFunders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -10,6 +10,7 @@ contract Faucet{
    
    uint public numOfFunders;
    mapping(address => bool)  private funders;
+   mapping(uint => address)  private lutFunders; // look up table funders
 
    // private -> can be accessible only within the smart contract
    // internal -> can be accessible within smart contract and also derived smart contract.
@@ -17,13 +18,28 @@ contract Faucet{
    receive() external payable{}
    function addFunds() external payable {
     address funder = msg.sender;
+
+    // if address already exists, we're not going to add it.
     if(!funders[funder]){
-        numOfFunders++;
+        uint index = numOfFunders++;
         funders[funder] = true;
+        lutFunders[index] = funder;
     }
    }
 
-   
+    function getAllFunders() external view returns(address[] memory){
+        address[] memory _funders = new address[](numOfFunders);
+
+        for(uint i=0;i<numOfFunders;i++){
+            _funders[i] = lutFunders[i];
+        } 
+
+        return _funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns(address){
+        return lutFunders[index];
+    }
    
     // pure, view - read-only call, no gas fee
     // view - it indicates that the function will not alter the storage state in any way
@@ -36,5 +52,8 @@ contract Faucet{
     // the block has gone through the proof of work. 8 bytes => 64bits 
 }
 
+
+// truffle migrate --reset
+// truffle console
 // const instance = await Faucet.deployed()
 // instance.addFunds({from: accounts[0], value: "20000000"})
